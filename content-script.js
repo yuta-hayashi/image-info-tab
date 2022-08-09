146efd88ed0ea389ab4228729d6ad2771411d5e1
yuta-hayashi <dev.hyuta@gmail.com>
2022-08-10 00:11:59 +0900

    fix format

--- content-script.js
+++ content-script.js
@@ -1,24 +1,26 @@
-const formatBytes = (bytes, decimals = 3)=> {
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+const formatBytes = (bytes, decimals = 3) => {
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
 
-const addInfoElement = (width, height, type, fileSize)=>{
+const addInfoElement = (width, height, type, fileSize) => {
   const div = document.createElement('div')
   const template = `
-  <span style='background-color: white; padding: 0.6rem;'>Width: ${width} px | Height: ${height} px | Type: ${type} | File size: ${formatBytes(fileSize)}</span>
+  <span style='background-color: white; padding: 0.6rem;'>Width: ${width} px | Height: ${height} px | Type: ${type} | File size: ${formatBytes(
+    fileSize
+  )}</span>
   `
   div.innerHTML = template
   document.body.insertBefore(div, document.body.firstChild)
 }
 
-if(document.body.childElementCount == 1){
+if (document.body.childElementCount == 1) {
   const imgElements = document.getElementsByTagName('img')
   const img = imgElements[0]
-  if(img){
+  if (img) {
     const xhr = new XMLHttpRequest()
     xhr.open('GET', img.src, true)
     xhr.responseType = 'blob'
